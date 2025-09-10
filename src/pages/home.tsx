5654da39ce6c90ee26e1e46baeb8393768952790
Sola Kabuta <solakabuta@gmail.com>
2025-09-11 01:42:02 +0200

    initial commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+// interface HomeProps {
+//   propName: type;
+// }
+
+const Home  = () => {
+  return (
+    <main>
+     <section>
+      <h1>This is the home page</h1> 
+     </section> 
+    </main>
+  );
+};
+
+export default Home;
