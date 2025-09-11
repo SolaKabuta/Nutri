a9d3e02bf3fc1133b6ddf50ecdab5e15c7b8851f
Sola Kabuta <solakabuta@gmail.com>
2025-09-11 02:16:54 +0200

    feat: colors update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,35 +1,48 @@
-import * as React from 'react';
-import { Button } from '../components/ui/button'
+import * as React from "react";
+import { Button } from "../components/ui/button";
 
 // interface HomeProps {
 //   propName: type;
 // }
 
-const Home  = () => {
+const Home = () => {
   return (
-    <main className='w-screen h-screen place-content-center'>
-      <div className='flex gap-10'>
-       {/* -- Hero Banner Container -- */}
-      <section>
-        <img src="/assets/images/hero_banner.jpg" alt="hero banner" />
-      </section>
-     <section>
-       {/* -- Title -- */}
-       <p className='text-primary underline underline-offset-8 font-semibold'>Transform Your ❤️ Health with</p>
-       <h1 className='text-4xl text-primary font-bold py-8'>Personalized Nutrition <br /> Coaching</h1> 
-       <div>
-       {/* -- Paragraph -- */}
-       <p>Welcome to Nutritionist, your partner in achieving optimal health through personalized nutrition</p>
-      <p>coaching. Our certified nutritionists are here to guide you on your weight loss journey, providing </p>
-      <p>customized plans and ongoing support. Start your transformation today and experience the </p>
-      <p>power of personalized nutrition coaching.</p>
-       </div>
-       {/* -- CTAs -- */}
-      <div className='flex gap-4 py-8'>
-      <Button>Get Starter Today</Button>
-      <Button variant="secondary">Book a demo</Button>
-      </div>
-     </section> 
+    <main className="w-screen h-screen place-content-center bg-accent-var">
+      <div className="flex gap-10 bg-title-background">
+        {/* -- Hero Banner Container -- */}
+        <section>
+          <img src="/assets/images/hero_banner.jpg" alt="hero banner" />
+        </section>
+        <section>
+          {/* -- Title -- */}
+          <p className="text-primary underline decoration-accent decoration-2 underline-offset-8 font-semibold">
+            Transform Your ❤️ Health with
+          </p>
+          <h1 className="text-4xl text-primary font-bold py-8">
+            Personalized Nutrition <br /> Coaching
+          </h1>
+          <div>
+            {/* -- Paragraph -- */}
+            <p>
+              Welcome to Nutritionist, your partner in achieving optimal health
+              through personalized nutrition
+            </p>
+            <p>
+              coaching. Our certified nutritionists are here to guide you on
+              your weight loss journey, providing{" "}
+            </p>
+            <p>
+              customized plans and ongoing support. Start your transformation
+              today and experience the{" "}
+            </p>
+            <p>power of personalized nutrition coaching.</p>
+          </div>
+          {/* -- CTAs -- */}
+          <div className="flex gap-4 py-8">
+            <Button>Get Starter Today</Button>
+            <Button variant="secondary">Book a demo</Button>
+          </div>
+        </section>
       </div>
     </main>
   );
