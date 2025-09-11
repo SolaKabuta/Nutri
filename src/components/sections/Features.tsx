fe844689512106600d6fed20fb6bc1f0dba37544
Sola Kabuta <solakabuta@gmail.com>
2025-09-11 18:47:01 +0200

    feat: features card json update

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -7,10 +7,15 @@ import FeaturesCard from "../ui/home/FeaturesCard";
 
 const Features = () => {
   return (
-    <main className="w-screen  grid place-content-center">
-     <h1 className="text-4xl py-8 font-bold text-center">Features</h1> 
-     <p className="pb-14">Welcome to the Feature Section of Nutritionist, your ultimate destination for all things nutrition and wellness.</p>
-     <FeaturesCard/>
+    <main className="w-screen grid place-content-center py-14">
+      <div className="text-center">
+        <h1 className="text-4xl py-8 font-bold">Features</h1>
+        <p className="pb-14">
+          Welcome to the Feature Section of Nutritionist, your ultimate
+          destination for all things nutrition and wellness.
+        </p>
+      </div>
+      <FeaturesCard />
     </main>
   );
 };
--- src/components/ui/home/FeaturesCard.tsx
+++ src/components/ui/home/FeaturesCard.tsx
@@ -2,21 +2,24 @@ import featuresCard from "../../../data/featuresCard.json";
 
 interface Feature {
   label: string;
+  icon: string;
   text: string[];
 }
 
 const FeaturesCard = () => {
   return (
-    <div className="bg-accent-var p-6">
+    <div className="">
+      <section className="grid grid-cols-2 gap-8 items-center">
       {featuresCard.map((feature: Feature, index:number) => (
-        <section key={index}>
+        <div key={index} className="bg-accent-var border-1 border-accent rounded-xl p-6">
           <div className="flex gap-3 items-center py-4">
-            <img src="/assets/icons/flower.svg" alt="flower icon" />
+            <img src={feature.icon} alt="features icon" />
             <p className="font-semibold">{feature.label}</p>
           </div>
           {feature.text.map((t, i) => <p key={i}>{t}</p>)}
-        </section>
+        </div>
       ))}
+      </section>
     </div>
   );
 };
