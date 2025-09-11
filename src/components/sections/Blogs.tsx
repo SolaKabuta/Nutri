c5c8a63ac620db0511c743ad3f8b6b73feb51a3e
Sola Kabuta <solakabuta@gmail.com>
2025-09-12 00:35:28 +0200

    feat: blogsCard.json added

---
+++ src/components/sections/Blogs.tsx
@@ -0,0 +1,23 @@
+// import { Button } from "../ui/button";
+// import FeaturesCard from "../ui/home/FeaturesCard";
+
+// interface HomeProps {
+//   propName: type;
+// }
+
+const Blogs = () => {
+  return (
+    <main className="w-screen grid place-content-center py-14">
+      <div className="text-center">
+        <h1 className="text-4xl py-8 font-bold">Our Blogs</h1>
+        <p className="pb-14">
+          Our blog is a treasure trove of informative and engaging articles written by our team of nutritionists, dietitians, and wellness <br />
+          experts. Here's what you can expect from our blog.
+        </p>
+      </div>
+      {/*<FeaturesCard />*/}
+    </main>
+  );
+};
+
+export default Blogs;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Hero from "../components/sections/Hero";
 import Features from "../components/sections/Features";
-// import Blogs from "../components/sections/Blogs";
+import Blogs from "../components/sections/Blogs";
 // import Testimonials from "../components/sections/Testimonials";
 // import Pricing from "../components/sections/Pricing";
 
@@ -14,7 +14,7 @@ const Home = () => {
     <>
       <Hero/>
       <Features/>
-      {/*<Blogs/>*/}
+      <Blogs/>
       {/*<Testimonials/>*/}
       {/*<Pricing/>*/}
     </>
