0e63f8c0081b37fba9c33803213c63d02461c322
Sola Kabuta <solakabuta@gmail.com>
2025-09-12 20:30:30 +0200

    style: carousel made kinda responsive

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -22,18 +22,17 @@ const Testimonials = () => {
     <main className="w-screen grid place-content-center py-14 px-24">
       <div className="text-center">
         <h1 className="text-4xl py-8 font-bold">Our Testimonials</h1>
-        <p className="pb-14">
-          Our satisfied clients share their success stories and experiences on
+        <p className="pb-14 text-sm md:text-base px-8 md:px-0">
+          Our satisfied clients share their success stories and <br className="md:hidden" /> experiences on
           their journey to better health and well-being.
         </p>
       </div>
-      <Carousel>
+      <Carousel className="w-[400px] md:w-[740px] lg:w-full">
         <CarouselContent >
           {testimonialsCard.map((item: Item, index: number) => (
-            <CarouselItem  className="basis-2/3 lg:basis-1/3">
+            <CarouselItem  className="basis-full lg:basis-1/3">
               <div key={index} className="relative bg-background border border-border-light h-[410px] rounded-xl p-10">
-                  
-                <img src={item.icon} alt="" />
+                <img src={item.icon} alt="quote icon" />
                 <div className="pt-12 pb-24">
                   <p>{item.text}</p>
                 </div>
