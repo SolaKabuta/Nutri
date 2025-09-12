4f6f63385e3dc97e81795a601bcefe3665bf54a1
Sola Kabuta <solakabuta@gmail.com>
2025-09-12 21:02:21 +0200

    style: cursor hover changed on testimonials cards

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -36,7 +36,7 @@ const Testimonials = () => {
       className=" w-[400px] md:w-[740px] lg:w-full">
         <CarouselContent >
           {testimonialsCard.map((item: Item, index: number) => (
-            <CarouselItem  className="basis-full lg:basis-1/3">
+            <CarouselItem  className="basis-full lg:basis-1/3 hover:cursor-grab active:cursor-grabbing">
               <div key={index} className="relative bg-background border border-border-light h-[410px] rounded-xl p-10">
                 <img src={item.icon} alt="quote icon" />
                 <div className="pt-12 pb-24">
