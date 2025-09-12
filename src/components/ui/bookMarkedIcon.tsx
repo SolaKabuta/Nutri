1a0c8d504bfba21168a3dd3309da0318763e63c3
Sola Kabuta <solakabuta@gmail.com>
2025-09-12 02:31:45 +0200

    feat: blogs card icons dynamic update

---
+++ src/components/ui/bookMarkedIcon.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { useState } from "react";
+
+
+interface BookMarkedIconProps {
+  propName: string;
+}
+
+const BookMarkedIcon: React.FC<BookMarkedIconProps> = ({ propName }) => {
+  
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  
+  return (
+    <div>
+      <button onClick={() => setIsBookmarked(prev => !prev)}>
+      {/* -- Fav Icon -- */}
+      <svg className={isBookmarked ? " fill-black" : "fill-none"} width={24} height={24} viewBox="0 0 24 24" fill="none">
+        <path
+          className="stroke-1"
+          d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+      </button>
+    </div>
+  );
+};
+
+export default BookMarkedIcon;
--- src/components/ui/home/BlogsCard.tsx
+++ src/components/ui/home/BlogsCard.tsx
@@ -1,5 +1,6 @@
 import blogsCard from "../../../data/blogsCard.json";
-import { useState } from "react";
+import BookMarkedIcon from "../bookMarkedIcon";
+import HeartIcon from "../heartIcon";
 
 interface Item {
   image: string;
@@ -12,7 +13,6 @@ interface Item {
 }
 
 const BlogsCard = () => {
-  const [isLiked, setIsLiked] = useState(false);
 
   return (
     <div className="">
@@ -41,33 +41,9 @@ const BlogsCard = () => {
                   </p>
                   <p>{item.dateRead}</p>
                 </div>
-                <section className="flex gap-3 items-center [&_div]:bg-accent-var [&_div]:p-2 [&_div]:rounded-lg">
-                  <div>
-                    {/* -- Heart Icon -- */}
-                    <svg width={24} height={24} viewBox="0 0 24 24" fill="none">
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M11.9932 5.13581C9.9938 2.7984 6.65975 2.16964 4.15469 4.31001C1.64964 6.45038 1.29697 10.029 3.2642 12.5604C4.89982 14.6651 9.84977 19.1041 11.4721 20.5408C11.6536 20.7016 11.7444 20.7819 11.8502 20.8135C11.9426 20.8411 12.0437 20.8411 12.1361 20.8135C12.2419 20.7819 12.3327 20.7016 12.5142 20.5408C14.1365 19.1041 19.0865 14.6651 20.7221 12.5604C22.6893 10.029 22.3797 6.42787 19.8316 4.31001C17.2835 2.19216 13.9925 2.7984 11.9932 5.13581Z"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div>
-                  <div>
-                  {/* -- Fav Icon -- */}
-                  <svg width={24} height={24} viewBox="0 0 24 24" fill="none">
-                    <path
-                      d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  </div>
+                <section className="flex gap-3 items-center [&_button]:bg-accent-var [&_button]:p-2 [&_button]:rounded-lg">
+                  <HeartIcon propName="" />
+                  <BookMarkedIcon propName="" />
                 </section>
               </section>
             </div>
