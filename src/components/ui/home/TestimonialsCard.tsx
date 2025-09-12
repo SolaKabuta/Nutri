7183757e25b1fac494d9a52eda03f62dc344a73a
Sola Kabuta <solakabuta@gmail.com>
2025-09-12 03:16:39 +0200

    style: testimonialscard layout update

--- src/components/ui/home/TestimonialsCard.tsx
+++ src/components/ui/home/TestimonialsCard.tsx
@@ -9,15 +9,20 @@ interface Item {
 
 const TestimonialsCard = () => {
   return (
-    <div className="">
-      <section className="grid grid-cols-2 gap-8 items-center">
+    <div className=" ">
+      <section className="flex gap-8 items-center">
       {testimonialsCard.map((item: Item, index:number) => (
-        <div key={index} className="bg-accent-var border-1 border-border-light rounded-xl p-6">
-          <div className="flex gap-3 items-center py-4">
+        <div key={index} className="relative bg-background border-1 border-border-light rounded-xl p-6">
+          <img src={item.icon} alt="" />
+          <div className="pt-12 pb-24">
+          {item.text.map((t, i) => 
+            <p key={i}>{t}</p>
+          )}
+          </div>
+          <div className="absolute bottom-0 bg-accent-var flex gap-3 items-center py-4 pr-50 mt-14">
             <img src={item.image} alt="features icon" />
             <p className="font-semibold">{item.name}</p>
           </div>
-          {item.text.map((t, i) => <p key={i}>{t}</p>)}
         </div>
       ))}
       </section>
