39c3d79db9acc75ad0039d7dfc7afa85a0e2d246
Sola Kabuta <solakabuta@gmail.com>
2025-09-13 03:30:19 +0200

    style: fighting with that footer layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,9 +16,9 @@ const menu = [
 
 const Nabar = () => {
   return (
-    <nav className="w-screen bg-primary text-white font-semibold py-4 px-8 lg:px-24">
+    <nav className="w-screen bg-primary  text-white font-semibold py-4 px-8 lg:px-24">
       {/* -- Navbar Upper Section -- */}
-      <div className="relative flex justify-center items-center gap-2 bg-navbar rounded-lg mb-2 p-2">
+      <div className="relative flex justify-center items-center gap-2 bg-navbar border border-price rounded-lg mb-2 p-2">
         <img className="absolute left-8 hidden md:block md:scale-75 lg:scale-100" src="/assets/images/abstract_left.png" alt="upper navbar left decoration" />
         <img className="absolute right-8 hidden md:block md:scale-75 lg:scale-100" src="/assets/images/abstract_right.png" alt="upper navbar left decoration" />
         <img src="/assets/icons/nav_ball_icon.svg" alt="navbar icon" />
