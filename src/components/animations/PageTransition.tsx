1b2c3a4a6636dc2e8d91cbc22d38432f4752de9e
Sola Kabuta <solakabuta@gmail.com>
2025-09-15 15:58:24 +0200

    style: pagetTransition animation update

--- src/components/animations/PageTransition.tsx
+++ src/components/animations/PageTransition.tsx
@@ -12,9 +12,9 @@ interface PageTransitionProps {
 export default function PageTransition({ children }: PageTransitionProps) {
   return (
     <motion.div
-      initial={{ opacity: 0.8, y: 100 }}   // au départ, caché et en bas
-      animate={{ opacity: 1, y: 0 }}     // à l’arrivée, visible et à sa place
-      exit={{ opacity: 0, y: -100 }}     // à la sortie, caché et en haut
+      initial={{ opacity: 0.8, x: 800 }}   // au départ, caché et en bas
+      animate={{ opacity: 1, x: 0 }}     // à l’arrivée, visible et à sa place
+      exit={{ opacity: 0, x: -800 }}     // à la sortie, caché et en haut
       transition={{ duration: 0.5, ease: "easeInOut" }}
       className="h-full w-full bg-background"
     >
