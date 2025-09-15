e44034b28b881343cc1e43f7b0cbad88e4e76f85
Sola Kabuta <solakabuta@gmail.com>
2025-09-15 11:45:37 +0200

    style: storycard layout update

--- src/components/ui/about/StoryCard.tsx
+++ src/components/ui/about/StoryCard.tsx
@@ -16,8 +16,8 @@ const StoryCard = () => {
           {/* -- Story Cards Left Column -- */}
           {storyCard.map((item: StoryCardProps, index: number) => (
             <div className={`flex flex-col md:flex-row gap-6 items-center ${index % 2 !== 0 ? "md:flex-row-reverse" : ""}`} key={index}>
-              <img className={`w-1/2 border-r-12 border-accent ${index % 2 !== 0 ? "border-none" : ""}`} src={item.image} alt={item.alt} width={400} height={400} />
-              <div className={`text-left ${index % 2 !== 0 ? "md:text-right" : ""}`}>
+              <img className={`w-1/2 border-b-12 md:border-b-0 md:border-r-12  border-accent ${index % 2 !== 0 ? "border-b-12 md:border-l-12 md:border-none " : ""}`} src={item.image} alt={item.alt} width={400} height={400} />
+              <div className={`w-1/2 text-left ${index % 2 !== 0 ? "md:text-right" : ""}`}>
                 <p className={`text-xl font-semibold underline underline-offset-8 decoration-accent`}>
                   {item.title}
                 </p>
